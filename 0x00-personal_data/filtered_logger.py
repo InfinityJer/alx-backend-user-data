28c7ee6c8e751b7ae2958253f1f8796b0cd4f7b0
InfinityJer <jclaris6@gmail.com>
2024-02-09 11:12:26 +0000

    updated filtered_logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -46,7 +46,7 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     """
     # Connect to MySQL database using environment variables
     db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = os.getenv("PERSONAL_DATA_DB_NAME", "")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME", "my_db")
     db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
     db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
     connection = mysql.connector.connect(
