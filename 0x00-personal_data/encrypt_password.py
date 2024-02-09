a9a124334e38214eecd933ebaee9c4ba0ab52716
InfinityJer <jclaris6@gmail.com>
2024-02-09 11:28:32 +0000

    6. Check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,4 +1,6 @@
-# encrypt_password.py
+#!/usr/bin/env python3
+"""password encryption module.
+"""
 
 import bcrypt
 
@@ -12,3 +14,9 @@ def hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode(), salt)
 
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks if the provided password matches the hashed password."""
+    # Use bcrypt to check if the provided password matches the hashed password
+    return bcrypt.checkpw(password.encode(), hashed_password)
