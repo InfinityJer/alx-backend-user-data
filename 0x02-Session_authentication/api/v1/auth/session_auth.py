32d05dc0e88d954e634d0ea36d6d3a911146dc61
InfinityJer <jclaris6@gmail.com>
2024-02-14 13:26:38 +0000

    Session authentication

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""Module for session authentication in the API.
+"""
+from uuid import uuid4
+from flask import request
+
+from .auth import Auth
+from models.user import User
+
+
+class SessionAuth(Auth):
+    """Class for handling session-based authentication.
+    """
+    # Dictionary to map session IDs to user IDs
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Generates a session ID for a given user.
+        """
+        if type(user_id) is str:
+            session_id = str(uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Retrieves the user ID associated with a given session ID.
+        """
+        if type(session_id) is str:
+            return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None) -> User:
+        """Retrieves the user associated with the current request.
+        """
+        user_id = self.user_id_for_session_id(self.session_cookie(request))
+        return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        """Terminates an authenticated session.
+        """
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        if (request is None or session_id is None) or user_id is None:
+            return False
+        if session_id in self.user_id_by_session_id:
+            del self.user_id_by_session_id[session_id]
+        return True
